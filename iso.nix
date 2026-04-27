d617d141a78dbeb01233933563fd4a7ad8af2f36
Maloschnikow <123018032+Maloschnikow@users.noreply.github.com>
2026-04-27 16:49:24 +0200

    add malte and marco ssh keys to root

--- iso.nix
+++ iso.nix
@@ -88,6 +88,8 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOV4f3/OcNQIHqomvH0nGLDmXDlrO/u7JKE9Fgq2Vuqs me@netali.de"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQD1kCdE0IWOt9Xg3J7PgkaDFQ1NWNRPM7dRy2R4sGm marc@schlagenhauf.net"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDoO+UrsXqPkkRX7C/qNsfw7Hmly1JuZe+gdh+2RYNfjPotLMkTvyYs7YeUmm55tP+jD8PCElMiix9e2jPGbBMX6b90iAh58E6f+AEtRQmTXAJouyj+NzHqSdlLd8ULYBnDftgOC9EpixN+Gjr7v9gkqskt0tT2feff9K49LRtFCqSOMilhlOGJ4ph6yaQ3P1oMv+EFTkUf3zBzXN7QsRE4epm6pfoAUe3ZJ4LMvAb8ki/jB0ywZpXVTeHN5w5cSYSYojdbo4xqHakzFGsw+uikiVCKwW6YBFblbQD73mRnvZoW3c3+F4NtY0tWakUjSrI1gTeVhjyQ4qlXM6bibPUNu9ZukTa9BzKFc2vwiSk7FKqlAXRF4WBWE8h/s+Fj4NCzVP9ebXRffQ7TAUg80ObuCwQcRSPsofzaHrb+/K7rAiLh4GymJHLb9pN7fJ3si0oezBpuXT9O1utoFMTi4wrIwVaTZhpHDMm6oDv3ZK7tRDrUtqLnWDlDWJMPhxN/nTRCWsmBPUa62kaEr9HFGntq7uosuLGmyn87pyLcb1/1Q9Cr4n+3k7m2f2XEXRmWNLFXmk7An0qu5dwQTUUO74jJK/tqViF5Cx6+laVPLXEDc1ru8rCZ3Ze0JsJ8g667qFfFRdRvKrHw8IkYzlAmDjZAoRU03JkDDoMqfpN7bCMyGw== lukas@yoga"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEegL8hoUE+rte/7PnivQ7DbgWLy8Pcg8srTcbxlfbke malte@think-top"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9wjlPCEdw5l3emedbuP7/9zqwC8CZHk2Tvs+pN5Ef/ marco@slycro"
   ];
 
   networking.dhcpcd.enable = false;
